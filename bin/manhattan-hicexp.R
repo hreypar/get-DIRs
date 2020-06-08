2294366a98f6f5d6a7837fc0755fc5ab986bd5a6
hreypar <hreypar@gmail.com>
2020-06-08 18:25:33 -0500

    manhattan hicexp function working properly

--- bin/manhattan-hicexp.R
+++ bin/manhattan-hicexp.R
@@ -6,21 +6,27 @@
 # Read in a normalized and compared hicexp object and produce a manhattan 
 # plot of the differences in a PDF format.
 #
+# always bear in mind that this plots REGIONS (not PAIRS of regions)
 #################### import libraries and set options ####################
 library(multiHiCcompare)
 library(optparse)
 #
+options(scipen = 10)
 ########################## functions ###################################
 # it's dangerous to go alone! take this.
 
 produce_manhattan <- function(comparison) {
-  pdf(file = paste0(opt$outputpath, gsub("\\.", "-", names(the.hicexp)), ".pdf"), 
-      height = 15, width = 30)
+  png(file = paste0(opt$outputpath, "/", gsub("\\.", "-", comparison), ".png"), 
+      height = 15, width = 30, units = "in", res = 500)
   
-  manhattan_hicexp(hicexp = the.hicexp[[comparison]])
-  title(gsub("\\.", " vs ", gsub("qlf.", "quasi-likelihood F-test ", names(comparison))))
-  abline(h = -log10(0.001), lwd=1, lty=3, col="red")
-  abline(h = -log10(0.05), lwd=1, lty=3, col="red")
+  manhattan_hicexp(hicexp = hicexp.comparison.list[[comparison]])
+  title(main = gsub("\\.", " vs ", gsub("qlf.", "quasi-likelihood F-test ", comparison)),
+        cex.main = 3)
+  abline(h = -log10(0.001), lwd=3, lty=2, col="red")
+  #abline(h = -log10(0.05), lwd=2.5, lty=2, col="red")
+  
+  pq = quantile(-log10(results(hicexp.comparison.list[[comparison]])$p.value))
+  axis(side = 2, at = seq(from = 0, to = round(pq["100%"]), by = 1), las=1)
   
   dev.off()
 }
@@ -29,10 +35,10 @@ produce_manhattan <- function(comparison) {
 option_list = list(
   make_option(opt_str = c("-i", "--input"), 
               type = "character",
-              help = "Input lis of hicexp objects as an Rds file"),
+              help = "A list (Rds object) of hicexp objects that have been compared"),
   make_option(opt_str = c("-o", "--outputpath"), 
               type = "character", 
-              help = "output filepath for PDF")
+              help = "filepath for output PDF")
 )
 opt <- parse_args(OptionParser(option_list=option_list))
 
@@ -41,9 +47,9 @@ if (is.null(opt$input)){
   stop("The input file is mandatory.n", call.=FALSE)
 }
 
-the.hicexp <- readRDS(opt$input)
+hicexp.comparison.list <- readRDS(opt$input)
 
 ########################## call plotting ###################################
-lapply(names(the.hicexp), produce_manhattan)
+lapply(names(hicexp.comparison.list), produce_manhattan)
 
 
