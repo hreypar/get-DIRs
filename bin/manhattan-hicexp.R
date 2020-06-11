e488dc60ae03518e0fcd9505e8a588dc021d2e46
hreypar <hreypar@gmail.com>
2020-06-10 21:06:55 -0500

    fix resolution calculation

--- bin/manhattan-hicexp.R
+++ bin/manhattan-hicexp.R
@@ -9,23 +9,34 @@
 # always bear in mind that this plots REGIONS (not PAIRS of regions)
 #################### import libraries and set options ####################
 library(multiHiCcompare)
-library(optparse)
 #
 options(scipen = 10)
 ########################## functions ###################################
 # it's dangerous to go alone! take this.
 
 produce_manhattan <- function(comparison) {
-  png(file = paste0(dirname(args), "/", gsub("\\.", "-", comparison), ".png"), 
+  # calculations
+    hicres = resolution(hicexp.comparison.list[[comparison]])
+  if(hicres >= 1000000) {
+    hicres = paste0(hicres/1000000, "Mb")
+  } else {
+    hicres = paste0(hicres/1000, "kb")
+  }
+  
+  pq = quantile(-log10(results(hicexp.comparison.list[[comparison]])$p.value))
+  
+  # create png
+  png(file = paste0(dirname(args), "/", gsub("\\.", "-", comparison), "-", hicres, ".png"), 
       height = 15, width = 30, units = "in", res = 500)
   
   manhattan_hicexp(hicexp = hicexp.comparison.list[[comparison]])
+  
   title(main = gsub("\\.", " vs ", gsub("qlf.", "quasi-likelihood F-test ", comparison)),
-        cex.main = 3, sub = paste0("(", resolution(comparison)/100, "kb resolution", ")"), cex.sub = 3)
+        cex.main = 3, sub = paste0("(", hicres, " resolution)"), cex.sub = 3)
+
   abline(h = -log10(0.001), lwd=3, lty=2, col="deepskyblue")
   abline(h = -log10(0.05), lwd=2.5, lty=2, col="blue")
   
-  pq = quantile(-log10(results(hicexp.comparison.list[[comparison]])$p.value))
   axis(side = 2, at = seq(from = 0, to = round(pq["100%"]), by = 1), las=1)
   
   dev.off()
